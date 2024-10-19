a8a3aa15b23b1bc887c17839f27f6ea855d741be
SaadNabeel <saadnabeel2010@gmail.com>
2024-10-19 14:52:13 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+a = {2, 5, 7, 12, 3, 23, 65, 42, 2, 3}  
+print('Set a:', a)
+
+a.add(67)  
+print('Updated Set a:', a)
+
+a.pop()  
+print('Updated Set a:', a)
+
+b = {2, 5, '7', 12, 13.6, 23, '65', 42, 2, 3.2}  
+print('Set b:', b)
+
+
